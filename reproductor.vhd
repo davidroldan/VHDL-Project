1f408838e6fc8bffff30838ef6a96f98972d0ab9
juanan <juanan@ad7b80be-2847-4137-a471-91bf83ddad43>
2014-01-06 19:22:58 +0000

    preliminar reproductor.vhd

--- reproductor.vhd
+++ reproductor.vhd
@@ -1,95 +1,91 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    13:51:20 12/19/2013 
--- Design Name: 
--- Module Name:    reproductor - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+
 use work.tipos.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity reproductor is	
-	port (clk : in std_logic;
-			clkdiv : in std_logic;
-			rst : in std_logic;
-			play : in std_logic;
-			addr : in std_logic_vector(9 downto 0);
-			memdir : out std_logic_vector (9 downto 0);
-			memdata : in std_logic_vector (15 downto 0);
-			fin : out std_logic;
-			onota	: out TNota;
-			ooctava : out std_logic_vector(2 downto 0);
-			osos : out std_logic
+	port (
+		-- Reloj de la FPGA
+		clk : in std_logic;
+		-- Reloj
+		clkdiv : in std_logic;
+		-- Reset
+		rst : in std_logic;
+		
+		-- Señal de control
+		play : in std_logic;
+			
+		-- dirección inicial de lectura
+		addr : in std_logic_vector(9 downto 0);
+			
+		-- Interacción con memoria
+		memdir : out std_logic_vector (9 downto 0);
+		memdata : in std_logic_vector (15 downto 0);
+		
+		-- Datos de salida
+		onota	: out TNota;
+		ooctava : out std_logic_vector(2 downto 0);
+		osos : out std_logic
+		
+		-- Señal de fin de reproducción
+		fin : out std_logic;
 	);
 			
 end reproductor;
 
 architecture Behavioral of reproductor is
-	type estado is (esperando, leyendo, reproduciendo);
+	type estado is (esperando, reproduciendo);
 	
 	signal estadoa, estadosig : estado;
 	signal tiempo : std_logic_vector( 7 downto 0);
-
-
+	signal dirsig : std_logic_vector(9 downto 0);
+	signal clkdiv_ant : std_logic;
+	
 begin
+	memdir <= dirsig;
+	
 	sincrono : process (clk, clkdiv, rst)
 	begin
 		if rst = '1' then
 			estadoa <= esperando;
 			tiempo <= (others => '0');
+			clkdiv_ant <= '0';
 			
 		elsif rising_edge(clk) then
+			clkdiv_ant <= clkdiv;
 			estadoa <= estadosig;
+			
 			case estadoa is
 				when esperando =>
-				
-				when leyendo =>
-				
-				when reproduciendo =>
+					dirsig <= addr;
+					tiempo <= (others => '0');			
 					
+				when reproduciendo =>
+					if tiempo = 0 then
+						dirsig <= dirsig + 1;
+						tiempo <= memdata(7 downto 0);
+						
+					elsif clkdiv_ant /= clkdiv and clkdiv = '1' then
+						tiempo <= tiempo - 1;
+	
+					end if;
 			end case;
 		end if;
 	end process sincrono;
 
-	combinacional : process (estadoa, play)
+
+	estadosig <= esperando when play = '0' OR memdata = 0
+				else reproduciendo;
 	
-	begin
-		if play = '1' then
-			case estadoa is
-				when esperando =>
-					estadosig <= leyendo;
-				when leyendo =>
-					estadosig <= reproduciendo;
-				when reproduciendo =>
-					estadosig <= leyendo;
-			end case;
-		else
-			estadosig <= esperando;
-		end if;
-		
-	end process combinacional;
+	with estadoa select
+		fin <= '1' when esperando
+				'0' when others;
+				
+				
+	onota <= memdata(14 downto 12)
+	ooctava <= memdata (11 downto 9);
+	osos <= memdata(8);
 	
 end Behavioral;
 
